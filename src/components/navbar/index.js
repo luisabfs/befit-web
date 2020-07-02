c1eff33fa7770a409f5347b5256861e3e04da684
Luísa Barros <qlullyq@gmail.com>
2020-07-02 02:23:53 -0300

    refactor: make brand a navmenu item

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-
-import NavMenu from './navmenu';
-import { Container } from './styles';
-
-import Brand from '../../assets/svg/brand@2x.svg';
+import { Container, Menu, Item } from './styles';
+import Brand from '../../assets/svg/brand-figma.svg';
 
 function Navbar() {
   return(
     <Container>
-      <img src={Brand} width="116px" />
-      <NavMenu />
+      <Menu>
+        <Item><img src={Brand} width="116px" /></Item>
+        <Item>home</Item>
+        <Item>motivação</Item>
+        <Item>como funciona</Item>
+      </Menu>
     </Container>
   );
 }
