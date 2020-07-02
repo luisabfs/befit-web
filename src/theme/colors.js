d3a1aa50ca663af06070df49b0990e7f7abe55e6
Luísa Barros <qlullyq@gmail.com>
2020-07-02 02:34:52 -0300

    feat: create colors theme

---
+++ src/theme/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#0097A7',
+  primaryLight: '#4DD0E1',
+  primaryDark: '#097B89',
+
+  accent: '#FF4081',
+  accentDark: '#F53967',
+
+  textLight: '#FAFAFA',
+  textDark: '#383838',
+}
