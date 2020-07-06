0bd1a631f0f96df0a38c72f0f0324852e68d3753
Luísa Barros <qlullyq@gmail.com>
2020-07-05 22:37:44 -0300

    feat: create header

--- src/sections/header/index.js
+++ src/sections/header/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Navbar } from '../../components';
-import { Container, Wrapper } from './styles';
+import { Navbar, Button } from '../../components';
+import Runner from '../../assets/images/man-running.png';
+import { Container, Wrapper, WrapCaption, Title, Subtitle } from './styles';
 
 function Header() {
   return (
     <Container>
       <Navbar />
       <Wrapper>
-        <h1>HEADER</h1>
+        <img src={Runner} />
+        <WrapCaption>
+          <Title>O futuro da atividade física é coletivo!</Title>
+          <Subtitle>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Autem, dolore est debitis, error repellendus.
+          </Subtitle>
+          <Button text="Cadastre-se" />
+        </WrapCaption>
       </Wrapper>
     </Container>
 
--- src/sections/header/styles.js
+++ src/sections/header/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
+import { colors } from '../../theme';
 
 export const Container = styled.div`
   display: flex;
-  flex: 1;
   flex-direction: column;
   height: 100vh;
 `;
@@ -10,4 +10,22 @@ export const Container = styled.div`
 export const Wrapper = styled.div`
   display: flex;
   padding: 100px;
+`;
+
+export const WrapCaption = styled.div`
+  display: flex;
+  max-width: 60%;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  font-size: 64px;
+  color: ${colors.textDark};
+`;
+
+export const Subtitle = styled.h3`
+  font-size: 22px;
+  margin-top: 10px;
+  font-weight: 400;
+  color: ${colors.textSecondary};
 `;
